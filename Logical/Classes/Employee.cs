be4ded281379e27c82c39c343fc5abef30232dd9
ikram101 <syed.ikramali@gmail.com>
2019-01-31 16:42:29 -0500

    1

---
+++ Logical/Classes/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logical.Classes
+{
+
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmpName { get; set; }
+
+        public int GetAge
+        {
+
+            get { return 12; }
+
+        }
+
+
+    }
+}
--- Logical/MyStrings.cs
+++ Logical/MyStrings.cs
@@ -166,20 +166,6 @@ namespace Logical
     }
 
 
-
-    public class Employee
-    {
-        public int EmployeeID { get; set; }
-        public string EmpName { get; set; }
-
-        public int GetAge
-        {
-
-            get { return 12; }
-
-        }
-    }
-
      
 
 }
