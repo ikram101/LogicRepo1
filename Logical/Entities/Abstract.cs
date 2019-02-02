00cf14bab872ef15875a81423401b42a0fc38ccb
ikram101 <syed.ikramali@gmail.com>
2019-02-02 02:04:30 -0500

    1

---
+++ Logical/Entities/Abstract.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logical
+{
+
+    public abstract class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmpName { get; set; }
+    }
+
+    public class ContractEmployee : Employee
+    {
+
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+
+    }
+
+
+    public class Test
+    {
+        ContractEmployee cEmp = new ContractEmployee();
+        cEmp.
+        
+    }
+
+}
---
+++ Logical/Entities/Static.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logical.Entities
+{
+    public static class Employee
+    {
+        public static int Empid { get; set; }
+
+        public static double GetAge(DateTime dob)
+        {
+            TimeSpan ts = DateTime.Now - dob;
+            var age = ts.TotalDays / 365;
+
+            return age;
+        }
+
+
+    }
+
+    //Error: Cannot dirive from a static class
+    //Error: Cannot declare instance method/pro in a static class
+
+    //public class ContractEmployee:Employee
+    //{
+
+    //}
+
+}
