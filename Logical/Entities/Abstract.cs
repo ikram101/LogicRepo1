40a8e71203a9a52b7485ece147dd749a6fc00298
ikram101 <syed.ikramali@gmail.com>
2019-02-02 21:08:56 -0500

    1

--- Logical/Entities/Abstract.cs
+++ Logical/Entities/Abstract.cs
@@ -9,24 +9,66 @@ namespace Logical
     {
         public int EmployeeID { get; set; }
         public string EmpName { get; set; }
+
+        public abstract DateTime DOB { get; set; }
+
+        public abstract void DoWork(int i);
+
+
+        public void GetAge(int i)
+        {
+
+        }
+        public virtual void GetAge2(int i)
+        {
+
+        }
+
     }
 
     public class ContractEmployee : Employee
     {
 
+        public override DateTime DOB { get; set; }
+
+        public override void DoWork(int i)
+        {
+            
+        }
+
+        public new void  GetAge(int i)
+        {
+
+        }
+
     }
 
     public class FullTimeEmployee : Employee
     {
+        public override DateTime DOB { get; set; }
 
+
+        public override void DoWork(int i)
+        {
+
+        }
+        public int ContractID { get; set; }
     }
 
 
-    public class Test
+     
+
+    public class Test1
     {
-        ContractEmployee cEmp = new ContractEmployee();
-        cEmp.
-        
+
+        public void hello()
+        {
+            FullTimeEmployee fe = new FullTimeEmployee();
+            
+
+
+         }
+
     }
 
 }
