23a2904322c5dfe9283e584d028bae5a483abee5
ikram101 <syed.ikramali@gmail.com>
2019-02-26 18:11:45 -0500

    my commit 002

--- Logical/MyRecursions.cs
+++ Logical/MyRecursions.cs
@@ -5,8 +5,10 @@ using System.Text;
 namespace Logical
 {
     public class MyRecursions
-    { 
+    {
         // my commit 001
+
+        // my commit 002
 
         public void Master01()
         {
